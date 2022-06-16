a58b6996dcb9d9de954191ad895372ca5de1ad6b
rex-smith <rex.smith17@gmail.com>
2022-06-16 17:43:19 -0400

    Added in beginning parts of ActionCable for messages

---
+++ app/javascript/channels/message_channel.js
@@ -0,0 +1,30 @@
+import consumer from "channels/consumer"
+
+consumer.subscriptions.create("MessageChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
+
+document.addEventListener("turbo:load", () => {
+  let form = document.querySelector('#message-form')
+  if(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      let messageInput = document.querySelector('#message-input').value
+      if(messageInput == '') return;
+      const message = {
+        body: messageInput
+      }
+      messageChannel.send({message: message})
+    })
+  }
+})
